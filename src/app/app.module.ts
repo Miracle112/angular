694369a5ae349174e06540159239779f48ddb440
Miracle112 <77540450+Miracle112@users.noreply.github.com>
2022-08-21 16:26:36 +0300

    Создание модального окна

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,26 @@ import { AppComponent } from './app.component';
 import {ProductComponent} from "./components/product/product.component";
 import {HttpClientModule} from "@angular/common/http";
 import { GlobalErrorComponents } from './components/global-error/global-error.components'
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {FilterProductsPipe} from "./components/pipes/filter-products.pipe";
+import {ModalComponent} from './components/modal/modal.component';
+import {CreateProductComponent} from "./components/create-product/create-product.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     GlobalErrorComponents,
-    FilterProductsPipe
+    FilterProductsPipe,
+    ModalComponent,
+    CreateProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
